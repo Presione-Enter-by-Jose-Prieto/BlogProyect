0db273cd97eb6ebe24d017d761d7637f371a151d
Jose Prieto <alejandroprietosal@gmail.com>
2025-12-27 15:41:31 -0500

    Lista de todos los posts en blog

---
+++ resources/views/app/blog/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('title', 'Blog')
+
+@section('content')
+    @if (session('success'))
+        <div class="">
+            {{ session('success') }}
+        </div>
+    @endif
+    <h2 class="text-xl font-bold mb-4">Bienvenido al Blog</h2>
+    <p class="mb-2">Este es el contenido principal del blog.</p>
+    <div>
+        @foreach ($posts as $post)
+            <div class="p-2 border mb-2">
+                <div class="flex flex-row gap-2 items-center">
+                    <h3 class="text-lg font-semibold">{{ $post->title }}</h3>
+                    <p class="text-sm text-gray-600">Publicado el {{ $post->created_at->format('d/m/Y') }}</p>
+                </div>
+                <p class="">{{ $post->excerpt }}</p>
+                <p class="text-sm text-gray-800">{{ $post->author->name ?? '—' }}</p>
+            </div>
+        @endforeach
+    </div>
+@endsection
